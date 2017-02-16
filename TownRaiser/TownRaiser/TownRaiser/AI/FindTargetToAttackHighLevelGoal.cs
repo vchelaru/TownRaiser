f1761fb2e757728819c3ad9d29f3ed09185e66dd
Victor Chelaru <VicChelaru@gmail.com>
2017-02-15 21:11:14 -0700

    Started adding aggro.

---
+++ TownRaiser/TownRaiser/TownRaiser/AI/FindTargetToAttackHighLevelGoal.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TownRaiser.Entities;
+
+namespace TownRaiser.AI
+{
+    public class FindTargetToAttackHighLevelGoal : HighLevelGoal
+    {
+        public Unit Owner { get; set; }
+
+        public List<Unit> AllUnits { get; set; }
+
+        public override void DecideWhatToDo()
+        {
+            const float aggroRadius = 80;
+            const float aggroSquared = aggroRadius * aggroRadius;
+            bool isTargetAnEnemyUnit = !Owner.UnitData.IsEnemy;
+
+            var found = AllUnits.FirstOrDefault(item => (item.Position - Owner.Position).LengthSquared() < aggroSquared);
+
+            if(found != null)
+            {
+                Owner.CreateAttackGoal(found);
+            }
+        }
+
+        public override bool GetIfDone()
+        {
+            return false;
+        }
+    }
+}
