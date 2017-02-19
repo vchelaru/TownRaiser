c56803a9b82a59755c77961bf7999683c093aeaf
Victor Chelaru <VicChelaru@gmail.com>
2017-02-18 22:51:09 -0700

    More work on attackmove.

--- TownRaiser/TownRaiser/TownRaiser/AI/AttackMoveHighLevelGoal.cs
+++ TownRaiser/TownRaiser/TownRaiser/AI/AttackMoveHighLevelGoal.cs
@@ -1,17 +1,76 @@
-﻿using System;
+﻿using FlatRedBall.Math;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TownRaiser.Entities;
 
 namespace TownRaiser.AI
 {
-    class AttackMoveHighLevelGoal
+    class AttackMoveHighLevelGoal : HighLevelGoal
     {
         AttackUnitHighLevelGoal attackGoal;
         WalkToHighLevelGoal walkGoal;
         FindTargetToAttackHighLevelGoal findTargetGoal;
 
+        Unit owner;
+        public Unit Owner
+        {
+            get
+            {
+                return owner;
+            }
+            set
+            {
+                owner = value;
+                findTargetGoal.Owner = owner;
+            }
+        }
 
+        PositionedObjectList<Unit> allUnits;
+        public PositionedObjectList<Unit> AllUnits
+        {
+            get { return allUnits; }
+            set
+            {
+                allUnits = value;
+                findTargetGoal.AllUnits = allUnits;
+            }
+        }
+
+        PositionedObjectList<Building> allBuildings;
+        public PositionedObjectList<Building> AllBuildings
+        {
+            get { return allBuildings; }
+            set
+            {
+                allBuildings = value;
+                findTargetGoal.AllBuildings = allBuildings;
+            }
+        }
+        
+
+        public AttackMoveHighLevelGoal()
+        {
+            CreateFindTargetGoal();
+        }
+
+        private void CreateFindTargetGoal()
+        {
+            findTargetGoal = new AI.FindTargetToAttackHighLevelGoal();
+
+            
+        }
+
+        public override bool GetIfDone()
+        {
+            return false;
+        }
+
+        public override void DecideWhatToDo()
+        {
+            findTargetGoal.DecideWhatToDo();
+        }
     }
 }
