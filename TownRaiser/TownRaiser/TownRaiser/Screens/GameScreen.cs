a2a472ff1f9955a8eb8ed1fdb9f22beaaf5fe7a7
Adam Patridge <patridge@gmail.com>
2017-03-08 22:03:27 -0700

    Shortcut All->Any in game end detection logic.

--- TownRaiser/TownRaiser/TownRaiser/Screens/GameScreen.cs
+++ TownRaiser/TownRaiser/TownRaiser/Screens/GameScreen.cs
@@ -440,7 +440,7 @@ namespace TownRaiser.Screens
 
         private void DetectEndGameActivity()
         {
-            if (EncounterSpawnPointList.All(spawnPt => spawnPt.CurrentLogicState == EncounterSpawnPoint.LogicState.Defeated))
+            if (!EncounterSpawnPointList.Any(spawnPt => spawnPt.CurrentLogicState != EncounterSpawnPoint.LogicState.Defeated))
             {
                 // Game over, man! Game over!
                 // TODO: Do something amazing!
